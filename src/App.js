f0b3a12904befe5849025fa1649c83d7efa3abc5
Apoorva1992 <78916880+Apoorva1992@users.noreply.github.com>
2022-11-07 10:20:30 +0530

    Day 3 | useEffect | forms | fetching data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Todos from './Todos';
+ import News from './News';
+//import TodosRehan from './TodosRehan';
 
 function App() {
+
+  // useEffect 
+  // Allows to perform side effects in your component 
+
+  let [count,setCount] = useState(0);
+  let [name,setName] = useState('John');
+
+  // This is without any dependency 
+  //Will run when the component is mounted as well as when componeny is updated
+  // useEffect(()=> {
+  //   console.log("component is loaded everytime");
+  //   //setCount(1); // the component re renders with updated values 
+  // });
+
+  // This is with empty dependency array 
+  //Will only run when the component is mounted   
+  // useEffect(()=>{
+  //   console.log("Hey I will only run once");
+  // },[])
+
+  // useEffect(()=>{
+  //   console.log("I wil only run when name is changed");
+  //   //setCount(count++);
+  // },[name])
+  // useEffect(()=>{
+  //   console.log("I wil only run when name is changed");
+  //   const interval = setInterval(()=> {
+  //     setCount(count++)
+  //   },1000);
+  //   // componnetWillUnmount- 
+  //   //cleanup
+  //   return () => clearInterval(interval);  
+  //   //setCount(count++);
+  // })
+  // useEffect(()=> {
+  //   console.log("load");
+  //   const interval = setInterval(()=>{
+  //     setCount((prev)=> prev +1);
+  //   },1000);
+  //   return () => clearInterval(interval);  
+  // })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {/* <News /> */}
+    <Todos />
+   
+    {/* <Todo />
+    <div className='text-7xl' >
+      {count}
+      <p>Hey my name is : {name}</p>
+      <button onClick={()=>{
+         setName("CHILL");
+         console.log("count", count)
+       setCount(count+ 1)
+      }}>Change Name</button>
+    </div> */}
+    </>
   );
 }
 
