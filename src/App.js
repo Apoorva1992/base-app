cca0abd1e855918dd8134b5677d55dc2ff501510
Apoorva1992 <78916880+Apoorva1992@users.noreply.github.com>
2022-11-11 10:43:48 +0530

    news app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from 'react';
+import { useDispatch , useSelector} from 'react-redux';
+//import { GET_NEWS } from './redux/reducers/action.types';
+import { getNews } from './redux/actions/news.action';
 function App() {
+  let { news, loading } = useSelector(state=>state.news)
+  const dispatch = useDispatch();
+
+  useEffect(()=> {
+    dispatch(getNews);
+    setTimeout(()=> {
+      console.log(news)
+    },5000)
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {loading ? "...Loading": news.articles.map((article)=> {
+        return(
+          <h1>{article.title}</h1>
+        )
+      })}
+      {/* {news?.articles.map((article)=> {
+        return(
+          <h1>{article.title}</h1>
+        )
+      })} */}
+     
     </div>
   );
 }
